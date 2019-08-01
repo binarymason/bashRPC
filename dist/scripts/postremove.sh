998450bf80e2caa6d050b206f8ca0e4e579556ad
John Mason <john@johnmason.io>
2019-08-01 11:10:29 -0400

    Initd fixes (#10)
    * stop service when uninstalling package
    
    * Use initd daemon func instead of nohup
    
    If a user were to try to install bashrpc over ssh
    where there is not a tty shell, then nohup is
    completely ineffective.

--- dist/scripts/postremove.sh
+++ dist/scripts/postremove.sh
@@ -2,6 +2,9 @@
 
 service_manager=$(ps --no-headers -o comm 1)
 
+# stop the bashrpc service if it's running
+service bashrpc stop &>/dev/null
+
 if [ "$service_manager" = init ]; then
   echo "+ removing init script"
   rm -f /etc/init.d/bashrpc
